7bbf0c8fd5046528bc1f47fe5f7d65a5584bebd8
Hafsa <99113724+HAFSA342@users.noreply.github.com>
2023-07-09 14:36:19 +0500

    chat app

---
+++ client/src/Components/ChatsPage.jsx
@@ -0,0 +1,20 @@
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "ace1e5d8-b6f3-4050-bba5-26ccde44e440",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+
+export default ChatsPage;
